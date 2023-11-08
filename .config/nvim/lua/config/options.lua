3caa162f74ba2ff863f3f10568fcbcbd86b164dd
johanwulf <johanwulf98@gmail.com>
2023-11-08 12:31:54 +0100

    refactor: update lazyvim and fix some issues

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,22 +1,23 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-local opt = vim.opt
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.autoindent = true
-opt.expandtab = true
-opt.mouse = ""
-opt.scrolloff = 15
+-- Defaults: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
+-- Lines to keep visible above/below cursor when scrolling
+vim.opt.scrolloff = 15
+
+-- Disable Netrw as we are using neo-tree.nvim
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = false,
-})
 
+-- Indenting
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+
+-- Hide command line when it is unused
+vim.opt.cmdheight = 0
+
+-- Show diagnostics on cursor hold
 vim.api.nvim_create_autocmd("CursorHold", {
-	buffer = bufnr,
 	callback = function()
 		local opts = {
 			focusable = false,
@@ -29,3 +30,8 @@ vim.api.nvim_create_autocmd("CursorHold", {
 		vim.diagnostic.open_float(nil, opts)
 	end,
 })
+
+-- Disable need to press enter when saving file
+vim.opt.shortmess = {
+	o = true,
+}
