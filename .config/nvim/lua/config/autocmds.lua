702c8b6f15f8f44e5c934f36127437a78273e30f
johanwulf <johanwulf98@gmail.com>
2023-11-13 16:51:38 +0100

    refactor: disable nvim lspconfig autoformat and enable prettier

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,1 +1,8 @@
 -- Defaults: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+
+-- Format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		vim.cmd(":Prettier")
+	end,
+})
