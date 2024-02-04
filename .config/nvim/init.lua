8875c1f7de871e6614384f358a030f486d8058db
Johan Wulf <johanwulf98@gmail.com>
2024-02-04 08:45:17 +0100

    feat: add prettier formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -78,6 +78,9 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  -- Autoformat
+  'prettier/vim-prettier',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
