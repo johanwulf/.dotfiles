6878293a00be515f7c67f87d70a2488e61e55280
Johan Wulf <johan.wulf@ingka.ikea.com>
2024-03-04 09:09:38 +0100

    chore: change colorscheme to catppuccin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -721,12 +721,13 @@ require('lazy').setup {
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin-macchiato'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
