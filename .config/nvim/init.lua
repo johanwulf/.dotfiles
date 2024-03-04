da26594f3e1a04bb27d3af60b5cdb33ea4c5943d
Johan Wulf <johan.wulf@ingka.ikea.com>
2024-03-04 11:53:36 +0100

    chore: further cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -288,10 +288,8 @@ require('lazy').setup {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        gopls = {},
         pyright = {},
         tsserver = {},
-
         lua_ls = {
           settings = {
             Lua = {
