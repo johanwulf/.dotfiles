a509e6f8e14f541fe0db1312e257cec768c066dc
Johan Wulf <johan.wulf@ingka.ikea.com>
2024-03-04 09:10:50 +0100

    chore: enable relative line numbers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -97,6 +97,7 @@ vim.g.maplocalleader = ' '
 
 -- Make line numbers default
 vim.opt.number = true
+vim.opt.relativenumber = true
 -- You can also add relative line numbers, for help with jumping.
 --  Experiment for yourself to see if you like it!
 -- vim.opt.relativenumber = true
