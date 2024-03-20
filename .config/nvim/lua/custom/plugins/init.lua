00dc942a95261bdfb7b26e8cee819dedc8ffbe82
Johan Wulf <johan.wulf@ingka.ikea.com>
2024-03-20 14:07:47 +0100

    feat(copilot): add github copilot for work folders

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,5 +1,8 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+-- Only enable copilot if in work folder
+local workPath = os.getenv 'WORK_PATH'
+if workPath and vim.fn.getcwd():find(workPath) then
+  return { 'github/copilot.vim' }
+else
+  print 'out'
+  return {}
+end
