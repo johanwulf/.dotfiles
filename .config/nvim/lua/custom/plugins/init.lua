348f7ed76f8376a836fc34a1b135512ee952f7dd
Johan wulf <johanwulf98@gmail.com>
2024-05-15 21:31:52 +0200

    feat: add oil.nvim for file navigation

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,7 +1,22 @@
 -- Only enable copilot if in work folder
-local workPath = os.getenv 'WORK_PATH'
-if workPath and vim.fn.getcwd():find(workPath) then
-  return { 'github/copilot.vim' }
-else
-  return {}
-end
+return {
+  { 'github/copilot.vim' },
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        columns = { 'icon' },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+      -- Open parent directory in floating window
+      vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+    end,
+  },
+}
