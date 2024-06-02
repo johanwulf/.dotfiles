074924c2f8e4fbe0e548cb2d2fd433b54224a3e5
Johan wulf <johanwulf98@gmail.com>
2024-06-02 17:02:11 +0200

    chore: remove Comment.nvim as gc is now built in

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -109,9 +109,6 @@ require('lazy').setup {
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   {
     'lewis6991/gitsigns.nvim',
