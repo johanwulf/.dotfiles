5d62fe9faeae3fad1ca08e9da2bddbfce83fac99
johanwulf <johanwulf98@gmail.com>
2025-09-18 14:27:47 +0200

    chore: remove error icons

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -262,14 +262,6 @@ require('lazy').setup({
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
         underline = { severity = vim.diagnostic.severity.ERROR },
-        signs = vim.g.have_nerd_font and {
-          text = {
-            [vim.diagnostic.severity.ERROR] = '󰅚 ',
-            [vim.diagnostic.severity.WARN] = '󰀪 ',
-            [vim.diagnostic.severity.INFO] = '󰋽 ',
-            [vim.diagnostic.severity.HINT] = '󰌶 ',
-          },
-        } or {},
         virtual_text = {
           source = 'if_many',
           spacing = 2,
